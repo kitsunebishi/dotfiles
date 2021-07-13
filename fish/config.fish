c0e051c75f843682da31ff778f62bf14f2182291
Raphael James <raphajamesfcunha@gmail.com>
2021-07-13 18:37:17 -0300

    ♡

---
+++ fish/config.fish
@@ -0,0 +1,15 @@
+set fish_greeting
+
+### "vim" as manpager
+# set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
+
+### RANDOM COLOR SCRIPT ###
+# Get this script from my GitLab: gitlab.com/dwt1/shell-color-scripts
+# Or install it from the Arch User Repository: shell-color-scripts
+ colorscript random 
+
+### neofetch ###
+#neofetch
+
+# the terminal rickroll
+alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash'
